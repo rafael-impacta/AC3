8b8c13d29e3616d29f2c1675ecdd939c05f38b11
Rafael Bernardino <69994519+rafael-impacta@users.noreply.github.com>
2020-10-11 16:57:36 -0300

    Delete Aumento de Salário.py

--- Aumento de Salário.py
+++
@@ -1,4 +0,0 @@
-salario = float(input())
-porc = float(input())
-novo = salario + ((salario * porc) / 100)
-print("Seu salario teve aumento de {} %, passando de R$ {} para R$ {}".format(porc, salario, novo))
